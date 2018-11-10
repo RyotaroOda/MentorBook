378c113ed352627b146f78f95e2a51002a06236e
小田　竜太郎 <odaryotaro@odaryuutarou-no-macbook-air.local>
2018-11-10 14:38:00 +0900

    Initial Commit

---
+++ MentorBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MentorBook
+//
+//  Created by 小田　竜太郎 on 2018/11/10.
+//  Copyright © 2018年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
